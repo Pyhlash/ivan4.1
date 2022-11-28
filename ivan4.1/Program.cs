a09551089158f5f4f0f47dd41795a961e81cd74c
Ivan lyashok <97744261+Pyhlash@users.noreply.github.com>
2022-11-28 22:21:04 +0300

    Добавьте файлы проекта.

---
+++ ivan4.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+public class MainClass
+{
+  public static void Main()
+  {
+    int month;
+    
+    restart:
+    WriteLine("Введите номер месяца");
+    month = int.Parse(Console.ReadLine());
+       
+        if(1<=month||12<=month)
+        {
+            
+            int lY = 2024;
+            int nLY = 2024;
+            Console.Write("год високосный?1 - да");
+            Console.WriteLine($"в месяце {DateTime.DaysInMonth(Console.ReadLine() == "1" ? nLY : lY, month)} дней");
+            goto restart;
+        }
+       
+        else
+        {
+            WriteLine("Такого месяца не существует");
+            goto restart;
+        }
+
+
+  }
+}
